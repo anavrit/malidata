37d069d526c34932e411f5e4798b3ad10b66f93e
Amit Prasad <amitprasad@Amits-MacBook-Pro.local>
2021-12-17 16:55:53 +0530

    Fixed untracked files

--- malidata/__init__.py
+++ malidata/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 def init_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+    Bootstrap(app)
 
     with app.app_context():
         from . import routes
