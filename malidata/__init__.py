21cb7c70559e5957e9ac80015a5253141e59e519
Amit Prasad <amitprasad@Amits-MacBook-Pro.local>
2021-12-17 23:36:13 +0530

    Added Bootstrap navbar

--- malidata/__init__.py
+++ malidata/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 def init_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
-    Bootstrap(app)
 
     with app.app_context():
         from . import routes
