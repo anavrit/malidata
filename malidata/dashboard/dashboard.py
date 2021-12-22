9c2d498acaf82191767d1381566cbdcbd016c805
Amit Prasad <amitprasad@Amits-MacBook-Pro.local>
2021-12-22 16:23:37 +0530

    Bootstrap loaded into Dashboard

--- malidata/dashboard/dashboard.py
+++ malidata/dashboard/dashboard.py
@@ -1,15 +1,17 @@
 import dash
 from dash import dcc, html
 from .dash_layout import html_layout
+import dash_bootstrap_components as dbc
 
 def init_dashboard(server):
     dash_app = dash.Dash(
         server = server,
         routes_pathname_prefix = "/dashboard/",
+        external_stylesheets=[dbc.themes.BOOTSTRAP],
     )
     dash_app.index_string = html_layout
 
-    dash_app.layout = html.Div([
-
-    ])
+    dash_app.layout = dbc.Container(
+        
+    )
     return dash_app.server
